2ce12196ffd93f59d4187f3393c9a109a4eb0bdc
vikas-gits-good <vikas.c.conappa@protonmail.com>
2026-01-24 11:22:07 +0530

    updated mongodb creds

--- src/Config/__init__.py
+++ src/Config/__init__.py
@@ -8,17 +8,23 @@ class SwiggyConfig:
         self.database = MongoDBConstants.SWIGGY.DATABASE_NAME
         self.coll_rstn_cnfg = MongoDBConstants.SWIGGY.COLLECTION_RESTAURANT_CONFIG
         self.coll_scrp_data = MongoDBConstants.SWIGGY.COLLECTION_SCRAPED_DATA
-        self.coll_rstn_menu = MongoDBConstants.SWIGGY.COLLECTION_RESTAURANT_MENU
+        self.coll_upst_fail = MongoDBConstants.SWIGGY.COLLECTION_UPSERT_FAIL
 
 
 class MongoDBConfig:
     def __init__(self):
         load_dotenv("src/Secrets/Database.env")
-        MONGO_USERNAME = os.getenv("MONGO_INITDB_ROOT_USERNAME")
-        MONGO_PASSWORD = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+        MONGO_USER = os.getenv("MONGO_INITDB_ROOT_USERNAME")
+        MONGO_PSWD = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+        MONGO_HOST = os.getenv("MONGO_HOST")
+        MONGO_PORT = os.getenv("MONGO_PORT")
+        MONGO_NAME = os.getenv("MONGO_NAME")
 
         self.mndb_conn_uri = MongoDBConstants.CONNECTION_URI.format(
-            username=MONGO_USERNAME,
-            password=MONGO_PASSWORD,
+            user=MONGO_USER,
+            pswd=MONGO_PSWD,
+            host=MONGO_HOST,
+            port=MONGO_PORT,
+            name=MONGO_NAME,
         )
         self.swiggy = SwiggyConfig()
--- src/Constants/__init__.py
+++ src/Constants/__init__.py
@@ -6,12 +6,10 @@ class SwiggyConstants:
     DATABASE_NAME = "Swiggy_Data"
     COLLECTION_RESTAURANT_CONFIG = "01_Restaurant_Config"
     COLLECTION_SCRAPED_DATA = "02_Scraped_JSON"
-    COLLECTION_RESTAURANT_MENU = "03_Restaurant_Menu_JSON"
+    COLLECTION_UPSERT_FAIL = "03_Upsert_Fail"
 
 
 @dataclass
 class MongoDBConstants:
-    CONNECTION_URI = (
-        "mongodb://{username}:{password}@localhost:27017/mongodb-local?authSource=admin"
-    )
+    CONNECTION_URI = "mongodb://{user}:{pswd}@{host}:{port}/{name}?authSource=admin"
     SWIGGY = SwiggyConstants()
