9c5408d56949a0ae2c93da9398476ddff2d466e3
vikas-gits-good <vikas.c.conappa@protonmail.com>
2026-01-28 20:07:20 +0530

    GRAG- added get_competitor_menu()

--- src/RAG/Config/tool_models.py
+++ src/RAG/Config/tool_models.py
@@ -2,7 +2,7 @@ from typing import Literal, List
 from pydantic import BaseModel, Field
 
 
-class GetCompetitorModels:
+class GetCompetitorDataModels:
     class QueryParams(BaseModel):
         area: str = Field(default="Koramangala", description="")
         cuisine: str = Field(default="Thai", description="")
@@ -10,7 +10,7 @@ class GetCompetitorModels:
         limit: int = Field(default=200, ge=1, le=2000, description="")
 
     class FunctionParams(BaseModel):
-        q_params: "GetCompetitorModels.QueryParams" = Field(description="")
+        q_params: "GetCompetitorDataModels.QueryParams" = Field(description="")
         output: Literal["dict", "dataframe"] = Field(default="dict", description="")
         exclude: List[
             Literal[
@@ -27,3 +27,34 @@ class GetCompetitorModels:
                 "city_id",
             ]
         ] = Field(default=["ids", "city_id"], description="")
+
+
+class GetCompetitorMenuModels:
+    class QueryParams(BaseModel):
+        area: str = Field(default="Koramangala", description="")
+        cuisine: str = Field(default="Thai", description="")
+        min_menu_rating: float = Field(default=4.0, ge=0.0, le=5.0, description="")
+        limit: int = Field(default=200, ge=1, le=2000, description="")
+
+    class FunctionParams(BaseModel):
+        q_params: "GetCompetitorMenuModels.QueryParams" = Field(description="")
+        output: Literal["dict", "dataframe"] = Field(default="dict", description="")
+        exclude: List[
+            Literal[
+                "rstn_ids",
+                "rstn_name",
+                "rstn_city",
+                "rstn_area",
+                "rstn_locality",
+                "rstn_cuisines",
+                "rstn_rating",
+                "rstn_address",
+                "rstn_coords",
+                "rstn_chain",
+                "rstn_city_id",
+                "food_name",
+                "food_types",
+                "food_price",
+                "food_rating",
+            ]
+        ] = Field(default=["rstn_ids", "rstn_city_id"], description="")
