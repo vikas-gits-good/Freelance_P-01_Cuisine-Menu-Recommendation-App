6341ca5212e5c5e983384f8586f07a302a427ad0
vikas-gits-good <vikas.c.conappa@protonmail.com>
2026-03-09 20:35:12 +0530

    added rag_api_call to state

--- Frontend/FE_Chat/Chat/state.py
+++ Frontend/FE_Chat/Chat/state.py
@@ -1,4 +1,9 @@
+import os
+from typing import Optional
+
+import httpx
 import reflex as rx
+from dotenv import load_dotenv
 from pydantic import BaseModel
 
 
@@ -17,6 +22,9 @@ class ChatMessage(BaseModel):
 class ChatState(rx.State):
     DID_SUBMT: bool = False
     CONVO_HIST: list[ChatMessage] = []
+    load_dotenv()
+    RAG_API_URL = os.getenv("RAG_API_URL", "")
+    THREAD_ID: Optional[str] = None
 
     @rx.var
     def user_form_submit(self) -> bool:
@@ -30,26 +38,38 @@ class ChatState(rx.State):
         self.clear_gui()
 
     def handle_submit(self, form_data: dict = {}):
-        user_message = form_data.get("HumanMessage", "")
-        if user_message:
-            self.DID_SUBMT = True
-            self.append_message_to_gui(user_message, False)
-            yield
-
-            # gpt_msgs = self.get_ai_messages()
-
-            # llm_rspn = llm_response(gpt_msgs)
-
-            self.DID_SUBMT = False
-            # self.append_message_to_gui(llm_rspn, True)
-            yield
-
-    def rag_request(self, user_message):
         try:
-            ...
+            user_message = form_data.get("HumanMessage", "")
+            if user_message:
+                self.DID_SUBMT = True
+                self.append_message_to_gui(user_message, False)
+                yield
+
+                # llm_rspn = self.rag_api_call(user_message)
+
+                self.DID_SUBMT = False
+                self.append_message_to_gui(llm_rspn, True)
+                yield
+
+        except Exception as e:
             pass
 
+    async def rag_api_call(self, message: str):
+        try:
+            payload = {"message": message, "thread_id": self.THREAD_ID}
+            async with httpx.AsyncClient() as client:
+                res = await client.post(
+                    f"{self.RAG_API_URL}/chat", json=payload, timeout=60
+                )
+                data = res.json()
 
+            self.THREAD_ID = data.get("thread_id", None)
+            response = data["reply"]
+
+        except Exception as e:
+            pass
+
+        return response
 
     def append_message_to_gui(
         self,
