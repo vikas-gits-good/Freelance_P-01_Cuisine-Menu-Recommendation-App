9abff87469c0bc6d8e21227a41b81ed59c98958a
vikas-gits-good <vikas.c.conappa@protonmail.com>
2026-03-11 11:06:32 +0530

    FE- updated rxconfig.py

--- Frontend/rxconfig.py
+++ Frontend/rxconfig.py
@@ -1,12 +1,17 @@
+import os
+
 import reflex as rx
+from dotenv import load_dotenv
+
+load_dotenv()
 
 config = rx.Config(
     app_name="FE_Chat",
     backend_host="0.0.0.0",
     backend_port=10001,
     frontend_port=10011,
-    api_url="http://localhost:10001",
-    deploy_url="http://localhost:10011",
+    api_url=os.getenv("RAG_APP_URL", "http://localhost"),
+    deploy_url=os.getenv("RAG_APP_URL", "http://localhost"),
     telemetry_enabled=False,
     state_auto_setters=False,
     plugins=[
